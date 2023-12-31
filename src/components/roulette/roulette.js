75b93bb000f91fbabacb6b3f6407f4276ae66020
Antoine Caudron <antoinecaudron8@gmail.com>
2023-12-31 20:07:10 +0100

    update numbersOfPart in roulette

--- src/components/roulette/roulette.js
+++ src/components/roulette/roulette.js
@@ -3,31 +3,40 @@ import React, { useEffect, useState } from "react";
 import Popup from "../popup/popup.js"
 import { Wheel } from "react-custom-roulette";
 
-const data = [
-    { option: "0" }, 
-    { option: "1" }, 
-    { option: "2" },
-    { option: "3" },
-    { option: "4" },
-    { option: "5" },
-    { option: "6" }
-];
-
 export default function Roulette() {
 
   const [players, setPlayers] = useState([]);
+  const [data, setData] = useState([])
   const [mustSpin, setMustSpin] = useState(false);
   const [prizeNumber, setPrizeNumber] = useState(0);
   const [buttonPopup, setButtonPopup] = useState(false);
   const [giveDrink, setGiveDrink] = useState(false)
+  const [numbersPart, setNumbersPart] = useState(JSON.parse(localStorage.getItem('numberspart')))
 
   useEffect(() => {
+    setData(data, [])
+
+    if (!JSON.parse(localStorage.getItem('numberspart')) > 0){
+      localStorage.setItem('numberspart', JSON.stringify(5));
+    }
+    
     const storedPlayers = JSON.parse(localStorage.getItem("players"));
 
     if (storedPlayers){
         setPlayers(storedPlayers)
     }
 
+    if (data.length == 0){
+      for(let i = 0; i<JSON.parse(localStorage.getItem('numberspart')); i++){
+        let texte = "Cul Sec"
+        if (i > 0){
+          texte = i
+        }
+        let object = {option: texte}
+        data.push(object)
+      }
+    }
+  
   }, []);
 
   const handleSpinClick = () => {
@@ -48,6 +57,15 @@ export default function Roulette() {
     setPlayers((prevPlayers) => [...prevPlayers.slice(1), prevPlayers[0]]);
   };
 
+  const handleChange = (event) => {
+    setNumbersPart(event.target.value)
+  }
+
+  const changePage = () => {
+    localStorage.setItem('numberspart', JSON.stringify(numbersPart));
+    window.location.reload();
+  }
+
   return (
     
     players.length === 0 ? (
@@ -56,7 +74,12 @@ export default function Roulette() {
             </div>
         ) :
 
+    
     <div className="wheelContent">
+      <div style={{display: "flex", gap: "15px", alignItems: "center"}}>
+        <label>Nombre de choix : </label><input onChange={handleChange}></input> 
+        <button onClick={changePage}>Change</button>
+      </div>
     <h2>C'est au tour de :</h2>
     <h1>{players[0]}</h1>
     <div>
